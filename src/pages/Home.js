0c8638657d484ac178481cf2c66bff658e78add5
clayhindman01 <clayhindman01@gmail.com>
2023-09-30 14:53:04 -0400

    First draft of accomodations page

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -3,16 +3,19 @@ import Header from "../components/Header";
 
 export default function Home() {
   return (
+    <>
+    <Header isHomepage={true} />
     <div className="container homepageContainer">
-      <Header />
-      <div className="center">
+      <div className="centerJustify homeheight">
         <p className="homeText">BOOK YOUR STAY WITH US TODAY</p>
         <div className="aboutUsButton">
-          <a href="/about" className="aboutUsText">
-            ABOUT US
+          <a href="/booking" className="aboutUsText">
+            BOOK NOW
           </a>
         </div>
       </div>
     </div>
+    </>
+    
   );
 }
