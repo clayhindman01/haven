e4a098ea1a02a8a37829f682cf3e44b9cde9343d
clayhindman01 <clayhindman01@gmail.com>
2023-10-02 09:56:57 -0400

    testaing

--- src/pages/Booking.js
+++ src/pages/Booking.js
@@ -1,6 +1,8 @@
 import React from "react";
 import Header from "../components/Header";
 import Cabin from "../components/Cabin";
+import { Tab, Tabs, TabList, TabPanel } from 'react-tabs';
+import 'react-tabs/style/react-tabs.css';
 
 export default function Booking() {
   return (
@@ -8,8 +10,18 @@ export default function Booking() {
     <Header isHomepage={false} />
     <div className="container bookingContainer">
       <div className="center">
-        <Cabin cabinOne={true} />
-        <Cabin cabinOne={false} />
+        <Tabs>
+          <TabList>
+            <Tab>Cabin One</Tab>
+            <Tab>Cabin Two</Tab>
+          </TabList>
+          <TabPanel>
+            <Cabin cabinOne={true} />
+          </TabPanel>
+          <TabPanel>
+            <Cabin cabinOne={false} />
+          </TabPanel>
+        </Tabs>
       </div>
     </div>
     </>
