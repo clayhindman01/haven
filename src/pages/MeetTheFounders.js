34facc171451b22a31e2ec7b4c7bbb0d82ae67f1
Clay Hindman <clayhindman01@gmail.com>
2024-01-25 11:48:17 -0500

    pushing

--- src/pages/MeetTheFounders.js
+++ src/pages/MeetTheFounders.js
@@ -10,20 +10,23 @@ export default function MeetTheFounders() {
         <div className="founderCon">
           <img src="founders.png" className="founderImage" />
           <div className="center meetTheFoundersText">
-            Welcome to our haven! <br /> We have been married for 25 years, and
-            each day has been an adventure. We have trekked parts of nearly
-            every National Park, and once we stop in Asia and Africa this Spring
-            we will have traveled to every continent! Throughout our travels, we
-            realized the most breathtaking moments were just the two of us,
-            enjoying the view, all alone. These moments have played a
-            significant role in our marriage! By allowing us to step away from
-            the chaos of life and the mundanity of routine, it reignites our
-            love for each other. We can reconnect in the middle of our
-            disconnected world. When we first stepped onto this breathtaking
-            acreage, we knew it was something special and we couldn't just keep
-            it to ourselves. We hope that as you retreat to The Haven, it will
-            be a place for you to experience rejuvenation and reconnection with
-            who matters most. <br /> ~ Ken & Julie
+            Welcome to our haven! <br />
+            <br /> We have been married for 25 years, and each day has been an
+            adventure. We have trekked parts of nearly every National Park, and
+            once we stop in Asia and Africa this Spring we will have traveled to
+            every continent! Throughout our travels, we realized the most
+            breathtaking moments were just the two of us, enjoying the view, all
+            alone. These moments have played a significant role in our marriage!
+            By allowing us to step away from the chaos of life and the mundanity
+            of routine, it reignites our love for each other. We can reconnect
+            in the middle of our disconnected world. <br />
+            <br />
+            When we first stepped onto this breathtaking acreage, we knew it was
+            something special and we couldn't just keep it to ourselves. We hope
+            that as you retreat to The Haven, it will be a place for you to
+            experience rejuvenation and reconnection with who matters most.
+            <br />
+            <br /> ~ Ken & Julie
           </div>
         </div>
         <Footer />
--- src/pages/Photos.js
+++ src/pages/Photos.js
@@ -8,19 +8,46 @@ export default function Photos() {
     <>
       <Header activeTab="photos" />
       <div className="container bookingContainer photoContainer">
-        <div className="center centerPhotos">
+        <div className=" centerPhotos">
           <Carousel axis="horizontal" autoPlay={true} dynamicHeight={true}>
             <div>
-              <img src="cabin.jpg" alt="cabin" className="carouselImage" />
+              <img src="juniper1.gif" className="carouselImage" />
             </div>
             <div>
-              <img src="cabin.jpg" alt="cabin" className="carouselImage" />
+              <img src="juniper2.jpg" className="carouselImage" />
             </div>
             <div>
-              <img src="cabin.jpg" alt="cabin" className="carouselImage" />
+              <img src="juniper3.webp" className="carouselImage" />
             </div>
             <div>
-              <img src="woods.jpg" alt="cabin" className="carouselImage" />
+              <img src="juniper4.webp" className="carouselImage" />
+            </div>
+            <div>
+              <img src="juniper5.webp" className="carouselImage" />
+            </div>
+            <div>
+              <img src="juniper6.webp" className="carouselImage" />
+            </div>
+            <div>
+              <img src="juniper7.webp" className="carouselImage" />
+            </div>
+            <div>
+              <img src="juniper8.webp" className="carouselImage" />
+            </div>
+            <div>
+              <img src="juniper9.webp" className="carouselImage" />
+            </div>
+            <div>
+              <img src="juniper10.webp" className="carouselImage" />
+            </div>
+            <div>
+              <img src="juniper11.webp" className="carouselImage" />
+            </div>
+            <div>
+              <img src="juniper12.webp" className="carouselImage" />
+            </div>
+            <div>
+              <img src="juniper13.webp" className="carouselImage" />
             </div>
           </Carousel>
         </div>
