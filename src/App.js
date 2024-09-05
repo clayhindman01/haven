f9e714dcae597a871a4c3b4292eff9fe023e9cd1
clayhindman01 <clayhindman01@gmail.com>
2024-09-05 17:10:17 -0400

    added analytics

--- src/App.js
+++ src/App.js
@@ -5,18 +5,22 @@ import Booking from "./pages/Booking";
 import Home from "./pages/Home";
 import Photos from "./pages/Photos";
 import MeetTheFounders from "./pages/MeetTheFounders";
+import { Analytics } from "@vercel/analytics/react";
 
 function App() {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/booking" element={<Booking />} />
-        <Route path="/weddingVenue" element={<WeddingVenue />} />
-        <Route path="/photos" element={<Photos />} />
-        <Route path="/about" element={<MeetTheFounders />} />
-      </Routes>
-    </BrowserRouter>
+    <>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/booking" element={<Booking />} />
+          <Route path="/weddingVenue" element={<WeddingVenue />} />
+          <Route path="/photos" element={<Photos />} />
+          <Route path="/about" element={<MeetTheFounders />} />
+        </Routes>
+      </BrowserRouter>
+      <Analytics />
+    </>
   );
 }
 
